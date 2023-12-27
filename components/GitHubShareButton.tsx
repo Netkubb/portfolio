4b64b3ddb57c0b2206bcc0870640f9290385506f
Netkubb <57309336+Netkubb@users.noreply.github.com>
2023-12-27 19:11:28 +0700

    add github side bar link to my github and give creadit to Travis Fischer at readme.md

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/Netkubb'
+      href='https://github.com/Netkubb/portfolio'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
