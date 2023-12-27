7223c793b716e3a94160eaf3aaa674bf89c3609f
Netkubb <57309336+Netkubb@users.noreply.github.com>
2023-12-27 22:52:45 +0700

    change canonical URL

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Net\'s Portfolio',
-  domain: 'netkubb.com',
+  domain: 'www.netkubb.com',
   author: 'Waranont Chaosanguan',
 
   // open graph metadata (optional)
