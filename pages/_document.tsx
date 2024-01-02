ea28cb5eecd6b4a1e655829bd87e0431042ad098
Netkubb <57309336+Netkubb@users.noreply.github.com>
2024-01-02 17:25:25 +0700

    change favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,13 +11,10 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.ico' />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='favicon.png'
-            />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
 
             <link rel='manifest' href='/manifest.json' />
           </Head>
