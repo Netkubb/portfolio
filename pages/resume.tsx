8c7994b7a48f2d9c629428296c872b85d40ae74f
Netkubb <57309336+Netkubb@users.noreply.github.com>
2024-01-03 17:08:30 +0700

    add resume again

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 function Resume() {
-    if(typeof window !== 'undefined'){
-        location.href = '/resume.pdf'
-    }
+    return <div style={{width:"100vw",height:"100vh",overflow:"hidden"}}>
+        <iframe src="/resume.pdf" width="100%" height="100%"/>
+    </div>
 }
 
 export default Resume;
