ccb892dcb3ea2dc07abebf85cd8a0640b9eeb4bf
Netkubb <57309336+Netkubb@users.noreply.github.com>
2024-01-03 17:17:15 +0700

    one last time

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Resume() {
     return <div style={{width:"100vw",height:"100vh",overflow:"hidden"}}>
-        <iframe src="/resume.pdf" width="100%" height="100%"/>
+        <iframe src="/Resume.pdf" width="100%" height="100%"/>
     </div>
 }
 
