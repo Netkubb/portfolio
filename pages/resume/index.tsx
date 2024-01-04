dbd4a1e198a7c3e0cdf41ab50ef4784dc0467c61
Netkubb <57309336+Netkubb@users.noreply.github.com>
2024-01-04 15:23:12 +0700

    add header for resume?

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,9 +1,15 @@
+import { PageHead } from '@/components/PageHead';
 import React from 'react';
 
+import * as config from '@/lib/config'
+
 function Resume() {
-    return <div style={{width:"100vw",height:"100vh",overflow:"hidden"}}>
-        <iframe src="/Resume.pdf" width="100%" height="100%"/>
-    </div>
+    return <>
+        <PageHead title="Resume" description="Resume" url={`${config.host}/resume`} image='https://images.unsplash.com/photo-1602407294553-6ac9170b3ed0?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+        <div style={{width:"100vw",height:"100vh",overflow:"hidden"}}>
+            <iframe src="/Resume.pdf" width="100%" height="100%"/>
+        </div>
+    </> 
 }
 
 export default Resume;
