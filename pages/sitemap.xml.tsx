40b1ccbd48f94d95b1ce12811fa7f093b1647365
Netkubb <57309336+Netkubb@users.noreply.github.com>
2024-01-06 02:23:33 +0700

    remove http

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -33,7 +33,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 
 const createSitemap = (siteMap: SiteMap) =>
   `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>${host}</loc>
     </url>
